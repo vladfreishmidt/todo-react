55390509515f02c177f611642e9378b6595a1e05
Vlad <vladfreishmidt.webdevelopment@gmail.com>
2021-02-17 18:38:33 +0200

    setState for todos is done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import React, { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import ToDoList from './components/ToDoList';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className='App'>
       <header>
         <h1>Vlad's ToDo List</h1>
       </header>
-      <Form />
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+      />
       <ToDoList />
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,27 @@
-const Form = () => {
+import React from 'react';
+
+const Form = ({ setInputValue, inputValue, todos, setTodos }) => {
+  const inputTextHandler = (e) => {
+    setInputValue(e.target.value);
+  };
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      { text: inputValue, completed: false, id: Math.random() * 1000 },
+    ]);
+    setInputValue('');
+  };
+
   return (
     <form>
-      <input type='text' className='todo-input' />
-      <button className='todo-button' type='submit'>
+      <input
+        type='text'
+        className='todo-input'
+        onChange={inputTextHandler}
+        value={inputValue}
+      />
+      <button onClick={submitTodoHandler} className='todo-button' type='submit'>
         <i className='fas fa-plus-square'></i>
       </button>
       <div className='select'>
